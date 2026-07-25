9693f74c7e2a2fa576f2c16dabc1a2bfd5e8171a
muebleria <mahorramas.2020@gmail.com>
2026-07-25 12:12:12 -0600

    conf

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,22 @@
+/* import type { NextConfig } from "next";
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+const allowedDevOrigins =
+  process.env.NEXT_ALLOWED_DEV_ORIGINS
+    ?.split(",")
+    .map(origin => origin.trim())
+    .filter(Boolean);
+
+const nextConfig: NextConfig = {
+  ...(isDevelopment &&
+    allowedDevOrigins &&
+    allowedDevOrigins.length > 0 && {
+      allowedDevOrigins,
+    }),
+};
+
+export default nextConfig; */
 import type { NextConfig } from "next";
 
 const isDevelopment = process.env.NODE_ENV === "development";
@@ -14,6 +33,25 @@ const nextConfig: NextConfig = {
     allowedDevOrigins.length > 0 && {
       allowedDevOrigins,
     }),
+
+  // ⬇️ AGREGA ESTA CONFIGURACIÓN DE IMÁGENES ⬇️
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "://muebleriasahorramas.com.mx",
+        port: "",
+        pathname: "/uploads/**",
+      },
+      // Esto te permite seguir probando de forma local en tu computadora
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
